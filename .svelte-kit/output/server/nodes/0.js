4a3f70efaed77b8ff50a2ff35d207d46f875d904
3v <ventic@3v.fi>
2022-07-07 15:45:44 +0300

    ignore svelte-kit

--- .svelte-kit/output/server/nodes/0.js
+++
@@ -1,7 +0,0 @@
-import * as module from '../entries/fallbacks/layout.svelte.js';
-
-export { module };
-export const index = 0;
-export const file = 'layout.svelte-c90fad07.js';
-export const imports = ["layout.svelte-c90fad07.js","chunks/index-165902b6.js"];
-export const stylesheets = [];
--- .svelte-kit/output/server/nodes/2.js
+++
@@ -1,7 +0,0 @@
-import * as module from '../entries/pages/index.svelte.js';
-
-export { module };
-export const index = 2;
-export const file = 'pages/index.svelte-cc4beede.js';
-export const imports = ["pages/index.svelte-cc4beede.js","chunks/index-165902b6.js","chunks/index-1dacbe9f.js"];
-export const stylesheets = ["assets/pages/index.svelte-a57f54c2.css"];
